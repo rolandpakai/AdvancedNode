7e9ae9cdc291e8b3ad0bd4d56dffb0c36d1e48a0
Roland Pákai <roland.pakai@liferay.com>
2023-03-12 13:08:25 +0100

    Add setupProxy.js

---
+++ client/src/setupProxy.js
@@ -0,0 +1,13 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use('/auth/**', createProxyMiddleware ({ 
+    target: 'http://localhost:5000/',
+    changeOrigin: false,
+  }));
+
+  app.use('/api/**', createProxyMiddleware ({ 
+    target: 'http://localhost:5000/',
+    changeOrigin: false,
+  }));
+};
