0f9fdcddfb7d6dd90937aa7de4afb6c1586a7ba8
Roland Pákai <roland.pakai@liferay.com>
2023-03-12 13:09:20 +0100

    User ReactDOM.createRoot

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -13,9 +13,11 @@ window.axios = axios;
 
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
