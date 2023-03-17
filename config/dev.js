c6520cfb74832f2a45d9bef692005258a7bf050f
Roland Pákai <roland.pakai@liferay.com>
2023-03-17 19:23:41 +0100

    Wiring Up Continuous Integration

---
+++ config/dev.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  redisUrl: process.env.REDIS_URL,
+};
--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,7 @@
-require("dotenv").config();
-
-module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./prod');
+} else if (process.env.NODE_ENV === 'ci') {
+  module.exports = require('./ci');
+} else {
+  module.exports = require('./dev');
+}
